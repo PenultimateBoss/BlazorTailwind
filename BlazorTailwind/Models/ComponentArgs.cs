cf19c581872787ea2ed9660cb0de00d4a5da8622
cispe <cispe@CISPE-DESKTOP>
2025-11-28 18:09:44 +0200

    feat: add base logic

---
+++ BlazorTailwind/Models/ComponentArgs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTailwind.Models;
+
+public sealed class ComponentArgs<TComponent> where TComponent : BaseComponent<TComponent>
+{
+    public string? Class { get; set; }
+    public TComponent This { get; set; } = null!;
+    public RenderFragment? ChildContent { get; set; }
+    public BaseComponent<TComponent> Base { get; set; } = null!;
+    public Dictionary<string, object?>? Attributes { get; set; }
+}
