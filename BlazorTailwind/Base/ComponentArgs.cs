80f779e5acff44338d955b048739d25accc1eeeb
cispe <cispe@CISPE-DESKTOP>
2025-12-01 13:15:10 +0200

    feat: relocate basic file to "Base" folder, add "id" and "style" properties

---
+++ BlazorTailwind/Base/ComponentArgs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTailwind.Base;
+
+public sealed class ComponentArgs<TComponent> where TComponent : ComponentBase<TComponent>
+{
+    public string? Id { get; set; }
+    public string? Class { get; set; }
+    public string? Style { get; set; }
+    public TComponent This { get; set; } = null!;
+    public RenderFragment? ChildContent { get; set; }
+    public ComponentBase<TComponent> Base { get; set; } = null!;
+    public Dictionary<string, object?>? Attributes { get; set; }
+}
---
+++ BlazorTailwind/Base/ComponentBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTailwind.Base;
+
+public class ComponentBase<TComponent> : ComponentBase where TComponent : ComponentBase<TComponent>
+{
+    [Parameter] public string? Id { get; set; }
+    [Parameter] public string? Class { get; set; }
+    [Parameter] public string? Style { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public RenderFragment<ComponentArgs<TComponent>>? RootElement { get; set; }
+    [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object?>? Attributes { get; set; }
+}
